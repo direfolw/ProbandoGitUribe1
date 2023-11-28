9471ba39d770d2d81e1b8c83565a3fcef1026a77
linarios <linarios512@hotmail.com>
2023-11-28 11:43:26 -0500

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    //Natillera de la abuela Consuelo
+    //¿? Qué necesito para procesar la natillera de 1 cliente?
+    //Entradas
+    int identificadorCliente = 233444;
+    long documentoIdentidad = 103651857977L;
+    long edadCliente = 17;
+    short cuotaMensualCliente = 25000;
+    float tasaInteresFijo=0.1f;
+    char calificacionCliente='A';
+
+    }
+
+}
